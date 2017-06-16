26fd495af34384c69cfd123f74f5f0ba043f6e93
Philip Putnam <putnamp989@gmail.com>
2017-06-15 21:11:20 -0700

    deleted unneccesary files, installed react-router-dom, cleaned up links, file naming conventions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+
           <h2>Welcome to React</h2>
+
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -17,5 +17,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
