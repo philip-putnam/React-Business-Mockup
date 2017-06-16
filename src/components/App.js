b1ff3a8f982afc6f66183c614e1942d28404bb26
Philip Putnam <putnamp989@gmail.com>
2017-06-16 15:15:59 -0700

    add simple Header component rendering at the top

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// Components
+import Header from './Header';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <Header />
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='Header'>
+      <span className='hamburger' />
+      <h3>Strex Corp.</h3>
+    </div>
+  )
+};
+
+export default Header;
